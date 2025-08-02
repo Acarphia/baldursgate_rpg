407c6d7a0c4ff475c9ae3a9ab1db61db1c310ce1
Acarphia <76790334+Acarphia@users.noreply.github.com>
2025-08-02 18:45:56 -0300

    Update FeiticeiroDraconico.java

--- Baldurs Gate/src/FeiticeiroDraconico.java
+++ Baldurs Gate/src/FeiticeiroDraconico.java
@@ -1,15 +1,25 @@
 public class FeiticeiroDraconico extends Feiticeiro {
+    private int cargasDraconicas;
+
     public FeiticeiroDraconico(Feiticeiro feiticeiro) {
         super(feiticeiro);
+        this.cargasDraconicas = 1;
         this.habilidades.add("Sopro Dracônico (1 carga)");
     }
 
     public void usarSoproDraconico() {
-        if (mana >= 20) {
+        if (cargasDraconicas > 0 && mana >= 20) {
+            cargasDraconicas--;
             mana -= 20;
             System.out.println(nome + " libera um devastador sopro dracônico!");
-        } else {
+        } else if (mana < 20) {
             System.out.println(nome + " não tem mana suficiente para usar o sopro dracônico!");
+        } else {
+            System.out.println(nome + " já usou o sopro dracônico e precisa recarregar!");
         }
+    }
+
+    public int getCargasDraconicas() {
+        return cargasDraconicas;
     }
 }

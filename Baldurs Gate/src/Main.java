967ef348ade2b022a4a67cce3e3158c37460bad6
marcosmarco <marcos13paulo02alves2006@gmail.com>
2025-08-02 14:09:30 -0300

    Update Main.java

--- Baldurs Gate/src/Main.java
+++ Baldurs Gate/src/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("- BALDUR'S GATE 3 -");
 
-        // Carga inicial de personagens
+        // Carga inicial de personagem base
         Personagem cargaInicial = criarPersonagemInicial();
         personagemAtual = cargaInicial;
 
@@ -72,15 +72,17 @@ public class Main {
         System.out.println("\n- PROMOÇÃO -");
         System.out.println("Classes disponíveis:");
         System.out.println("1. Guerreiro");
-        System.out.println("2. Barbáro");
+        System.out.println("2. Guerreiro Campeão");
         System.out.println("3. Assassino");
-        System.out.println("4. Clérigo");
-        System.out.println("5. Mago");
-        System.out.println("6. Feiticeiro");
-        System.out.println("7. Bruxo");
-        System.out.println("8. Bardo");
-        System.out.println("9. Bardo (Colégio da Sabedoria)");
-        System.out.println("10. Clérigo (Domínio da Luz)");
+        System.out.println("4. Assassino Sombrio");
+        System.out.println("5. Feiticeiro");
+        System.out.println("6. Feiticeiro Dracônico");
+        System.out.println("7. Mago");
+        System.out.println("8. Mago Evocador");
+        System.out.println("9. Bardo");
+        System.out.println("10. Bardo (Colégio da Sabedoria)");
+        System.out.println("11. Clérigo");
+        System.out.println("12. Clérigo (Domínio da Luz)");
         System.out.print("Escolha uma classe: ");
 
         int escolha = scanner.nextInt();
@@ -88,37 +90,59 @@ public class Main {
 
         switch (escolha) {
             case 1:
-                personagemAtual = personagemAtual.promoverParaGuerreiro();
+                personagemAtual = new Guerreiro(personagemAtual);
                 break;
             case 2:
-                personagemAtual = personagemAtual.promoverParaBarbaro();
+                if (personagemAtual instanceof Guerreiro) {
+                    personagemAtual = new GuerreiroCampeão((Guerreiro) personagemAtual);
+                } else {
+                    System.out.println("Apenas Guerreiros podem virar Campeões!");
+                }
                 break;
             case 3:
-                personagemAtual = personagemAtual.promoverParaAssassino();
+                personagemAtual = new Assassino(personagemAtual);
                 break;
             case 4:
-                personagemAtual = personagemAtual.promoverParaClerigo();
+                if (personagemAtual instanceof Assassino) {
+                    personagemAtual = new AssassinoSombrio((Assassino) personagemAtual);
+                } else {
+                    System.out.println("Apenas Assassinos podem virar Sombrio!");
+                }
                 break;
             case 5:
-                personagemAtual = personagemAtual.promoverParaMago();
+                personagemAtual = new Feiticeiro(personagemAtual);
                 break;
             case 6:
-                personagemAtual = personagemAtual.promoverParaFeiticeiro();
+                if (personagemAtual instanceof Feiticeiro) {
+                    personagemAtual = new FeiticeiroDraconico((Feiticeiro) personagemAtual);
+                } else {
+                    System.out.println("Apenas Feiticeiros podem virar Dracônicos!");
+                }
                 break;
             case 7:
-                personagemAtual = personagemAtual.promoverParaBruxo();
+                personagemAtual = new Mago(personagemAtual);
                 break;
             case 8:
-                personagemAtual = personagemAtual.promoverParaBardo();
+                if (personagemAtual instanceof Mago) {
+                    personagemAtual = new MagoEvocador((Mago) personagemAtual);
+                } else {
+                    System.out.println("Apenas Magos podem virar Evocadores!");
+                }
                 break;
             case 9:
+                personagemAtual = new Bardo(personagemAtual);
+                break;
+            case 10:
                 if (personagemAtual instanceof Bardo) {
                     personagemAtual = new BardoColegioSabedoria((Bardo) personagemAtual);
                 } else {
                     System.out.println("Apenas Bardos podem entrar no Colégio da Sabedoria!");
                 }
                 break;
-            case 10:
+            case 11:
+                personagemAtual = new Clerigo(personagemAtual);
+                break;
+            case 12:
                 if (personagemAtual instanceof Clerigo) {
                     personagemAtual = new ClerigoDominioLuz((Clerigo) personagemAtual);
                 } else {
@@ -132,59 +156,161 @@ public class Main {
 
     private static void usarHabilidades() {
         System.out.println("\n- HABILIDADES -");
-        if (personagemAtual instanceof ClerigoDominioLuz) {
-            ClerigoDominioLuz clerigoLuz = (ClerigoDominioLuz) personagemAtual;
-            System.out.println("1. Invocar Chama Sagrada");
-            System.out.println("2. Barreira de Luz");
-            System.out.println("3. Curar");
+
+        if (personagemAtual instanceof GuerreiroCampeão) {
+            GuerreiroCampeão gCamp = (GuerreiroCampeão) personagemAtual;
+            System.out.println("1. Usar Fúria");
+            System.out.println("2. Usar Golpe Crítico");
             System.out.print("Escolha: ");
             int escolha = scanner.nextInt();
 
             if (escolha == 1) {
-                clerigoLuz.invocarChamaSagrada();
+                gCamp.usarFuria();
             } else if (escolha == 2) {
-                System.out.println(clerigoLuz.getNome() + " cria uma barreira de luz!");
+                gCamp.usarGolpeCritico();
+            }
+        }
+        else if (personagemAtual instanceof Guerreiro) {
+            Guerreiro g = (Guerreiro) personagemAtual;
+            System.out.println("1. Usar Fúria");
+            System.out.println("2. Atacar");
+            System.out.print("Escolha: ");
+            int escolha = scanner.nextInt();
+
+            if (escolha == 1) {
+                g.usarFuria();
             } else {
-                clerigoLuz.curar();
+                g.atacar();
+            }
+        }
+        else if (personagemAtual instanceof AssassinoSombrio) {
+            AssassinoSombrio aS = (AssassinoSombrio) personagemAtual;
+            System.out.println("1. Ataque Furtivo");
+            System.out.println("2. Dança das Sombras");
+            System.out.print("Escolha: ");
+            int escolha = scanner.nextInt();
+
+            if (escolha == 1) {
+                aS.ataqueFurtivo();
+            } else {
+                aS.dancarSombras();
+            }
+        }
+        else if (personagemAtual instanceof Assassino) {
+            Assassino a = (Assassino) personagemAtual;
+            System.out.println("1. Ataque Furtivo");
+            System.out.println("2. Atacar");
+            System.out.print("Escolha: ");
+            int escolha = scanner.nextInt();
+
+            if (escolha == 1) {
+                a.ataqueFurtivo();
+            } else {
+                a.atacar();
+            }
+        }
+        else if (personagemAtual instanceof FeiticeiroDraconico) {
+            FeiticeiroDraconico fD = (FeiticeiroDraconico) personagemAtual;
+            System.out.println("1. Conjurar Raio de Caos");
+            System.out.println("2. Usar Sopro Dracônico");
+            System.out.print("Escolha: ");
+            int escolha = scanner.nextInt();
+
+            if (escolha == 1) {
+                fD.conjurarRaioCaos();
+            } else {
+                fD.usarSoproDraconico();
+            }
+        }
+        else if (personagemAtual instanceof Feiticeiro) {
+            Feiticeiro f = (Feiticeiro) personagemAtual;
+            System.out.println("1. Conjurar Raio de Caos");
+            System.out.println("2. Atacar");
+            System.out.print("Escolha: ");
+            int escolha = scanner.nextInt();
+
+            if (escolha == 1) {
+                f.conjurarRaioCaos();
+            } else {
+                f.atacar();
+            }
+        }
+        else if (personagemAtual instanceof MagoEvocador) {
+            MagoEvocador mE = (MagoEvocador) personagemAtual;
+            System.out.println("1. Lançar Mísseis");
+            System.out.println("2. Evocar Elemental");
+            System.out.print("Escolha: ");
+            int escolha = scanner.nextInt();
+
+            if (escolha == 1) {
+                mE.lancarMisseis();
+            } else {
+                mE.evocarElemental();
+            }
+        }
+        else if (personagemAtual instanceof Mago) {
+            Mago m = (Mago) personagemAtual;
+            System.out.println("1. Lançar Mísseis");
+            System.out.println("2. Atacar");
+            System.out.print("Escolha: ");
+            int escolha = scanner.nextInt();
+
+            if (escolha == 1) {
+                m.lancarMisseis();
+            } else {
+                m.atacar();
+            }
+        }
+        else if (personagemAtual instanceof ClerigoDominioLuz) {
+            ClerigoDominioLuz cL = (ClerigoDominioLuz) personagemAtual;
+            System.out.println("1. Invocar Chama Sagrada");
+            System.out.println("2. Curar");
+            System.out.print("Escolha: ");
+            int escolha = scanner.nextInt();
+
+            if (escolha == 1) {
+                cL.invocarChamaSagrada();
+            } else {
+                cL.curar();
+            }
+        }
+        else if (personagemAtual instanceof Clerigo) {
+            Clerigo c = (Clerigo) personagemAtual;
+            System.out.println("1. Curar ferimentos");
+            System.out.println("2. Atacar");
+            System.out.print("Escolha: ");
+            int escolha = scanner.nextInt();
+
+            if (escolha == 1) {
+                c.curar();
+            } else {
+                c.atacar();
             }
         }
         else if (personagemAtual instanceof BardoColegioSabedoria) {
-            BardoColegioSabedoria bardoSabedoria = (BardoColegioSabedoria) personagemAtual;
+            BardoColegioSabedoria bC = (BardoColegioSabedoria) personagemAtual;
             System.out.println("1. Palavra Cortante");
             System.out.println("2. Inspiração Superior");
             System.out.print("Escolha: ");
             int escolha = scanner.nextInt();
 
             if (escolha == 1) {
-                bardoSabedoria.cortantePalavra();
+                bC.cortantePalavra();
             } else {
-                bardoSabedoria.inspirar();
-            }
-        }
-        else if (personagemAtual instanceof Clerigo) {
-            Clerigo clerigo = (Clerigo) personagemAtual;
-            System.out.println("1. Curar ferimentos");
-            System.out.println("2. Atacar com símbolo");
-            System.out.print("Escolha: ");
-            int escolha = scanner.nextInt();
-
-            if (escolha == 1) {
-                clerigo.curar();
-            } else {
-                clerigo.atacar("símbolo");
+                bC.inspirar();
             }
         }
         else if (personagemAtual instanceof Bardo) {
-            Bardo bardo = (Bardo) personagemAtual;
+            Bardo b = (Bardo) personagemAtual;
             System.out.println("1. Inspirar aliados");
-            System.out.println("2. Atacar com alaúde");
+            System.out.println("2. Atacar");
             System.out.print("Escolha: ");
             int escolha = scanner.nextInt();
 
             if (escolha == 1) {
-                bardo.inspirar();
+                b.inspirar();
             } else {
-                bardo.atacar("alaúde");
+                b.atacar();
             }
         }
         else {
@@ -208,24 +334,56 @@ public class Main {
         System.out.println("\n- INFORMAÇÕES DO PERSONAGEM -");
         System.out.println("Nome: " + personagemAtual.getNome());
         System.out.println("Raça: " + personagemAtual.getRaca());
-        System.out.println("Vida: " + personagemAtual.getVida());
-        System.out.println("Mana: " + personagemAtual.getMana());
+        System.out.println("Vida: " + personagemAtual.getVida() + "/" + personagemAtual.getVidaMax());
+        System.out.println("Mana: " + personagemAtual.getMana() + "/" + personagemAtual.getManaMax());
 
-        if (personagemAtual instanceof ClerigoDominioLuz) {
+        if (personagemAtual instanceof GuerreiroCampeão) {
+            System.out.println("Classe: Guerreiro Campeão");
+            System.out.println("Fúria restante: " + ((GuerreiroCampeão) personagemAtual).getFuria());
+        }
+        else if (personagemAtual instanceof Guerreiro) {
+            System.out.println("Classe: Guerreiro");
+            System.out.println("Fúria restante: " + ((Guerreiro) personagemAtual).getFuria());
+        }
+        else if (personagemAtual instanceof AssassinoSombrio) {
+            System.out.println("Classe: Assassino Sombrio");
+            System.out.println("Sombras restantes: " + ((AssassinoSombrio) personagemAtual).getSombras());
+        }
+        else if (personagemAtual instanceof Assassino) {
+            System.out.println("Classe: Assassino");
+            System.out.println("Furtividade restante: " + ((Assassino) personagemAtual).getFurtividade());
+        }
+        else if (personagemAtual instanceof FeiticeiroDraconico) {
+            System.out.println("Classe: Feiticeiro Dracônico");
+            System.out.println("Mana restante: " + personagemAtual.getMana());
+        }
+        else if (personagemAtual instanceof Feiticeiro) {
+            System.out.println("Classe: Feiticeiro");
+            System.out.println("Mana restante: " + personagemAtual.getMana());
+        }
+        else if (personagemAtual instanceof MagoEvocador) {
+            System.out.println("Classe: Mago Evocador");
+            System.out.println("Evocações restantes: " + ((MagoEvocador) personagemAtual).getEvocacoes());
+        }
+        else if (personagemAtual instanceof Mago) {
+            System.out.println("Classe: Mago");
+            System.out.println("Livros restantes: " + ((Mago) personagemAtual).getLivros());
+        }
+        else if (personagemAtual instanceof ClerigoDominioLuz) {
             System.out.println("Classe: Clérigo (Domínio da Luz)");
             System.out.println("Divindade: " + ((Clerigo) personagemAtual).getDivindade());
-        }
-        else if (personagemAtual instanceof BardoColegioSabedoria) {
-            System.out.println("Classe: Bardo (Colégio da Sabedoria)");
-            System.out.println("Inspirações: " + ((Bardo) personagemAtual).getInspiracao());
         }
         else if (personagemAtual instanceof Clerigo) {
             System.out.println("Classe: Clérigo");
             System.out.println("Divindade: " + ((Clerigo) personagemAtual).getDivindade());
         }
+        else if (personagemAtual instanceof BardoColegioSabedoria) {
+            System.out.println("Classe: Bardo (Colégio da Sabedoria)");
+            System.out.println("Inspiração superior restante: " + ((BardoColegioSabedoria) personagemAtual).getInspiracaoSuperior());
+        }
         else if (personagemAtual instanceof Bardo) {
             System.out.println("Classe: Bardo");
-            System.out.println("Inspirações: " + ((Bardo) personagemAtual).getInspiracao());
+            System.out.println("Inspiração restante: " + ((Bardo) personagemAtual).getInspiracao());
         }
         else {
             System.out.println("Classe: Aventureiro (não especializado)");

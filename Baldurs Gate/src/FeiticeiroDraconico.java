3521776eb2e40e8f5ba5f56a3e43948ac36f955b
Acarphia <76790334+Acarphia@users.noreply.github.com>
2025-08-02 18:55:13 -0300

    Update FeiticeiroDraconico.java

--- Baldurs Gate/src/FeiticeiroDraconico.java
+++ Baldurs Gate/src/FeiticeiroDraconico.java
@@ -1,12 +1,14 @@
+// Subclasse Feiticeiro Dracônico (herda de Feiticeiro)
 public class FeiticeiroDraconico extends Feiticeiro {
-    private int cargasDraconicas;
+    private int cargasDraconicas; // Cargas do Sopro Dracônico
 
     public FeiticeiroDraconico(Feiticeiro feiticeiro) {
         super(feiticeiro);
-        this.cargasDraconicas = 1;
+        this.cargasDraconicas = 1; // Inicia com 1 carga
         this.habilidades.add("Sopro Dracônico (1 carga)");
     }
 
+    // Método único de subclasse
     public void usarSoproDraconico() {
         if (cargasDraconicas > 0 && mana >= 20) {
             cargasDraconicas--;
@@ -19,7 +21,22 @@ public class FeiticeiroDraconico extends Feiticeiro {
         }
     }
 
+    // Sobrescrita
+    @Override
+    public void conjurarRaioCaos() {
+        if (cargasDraconicas > 0 && mana >= 15) {
+            cargasDraconicas--;
+            mana -= 15;
+            System.out.println(nome + " conjura um Raio de Caos dracônico poderoso!");
+        } else if (mana < 15) {
+            System.out.println(nome + " não tem mana suficiente para conjurar o Raio de Caos dracônico!");
+        } else {
+            System.out.println(nome + " já usou o Raio de Caos dracônico e precisa recarregar!");
+        }
+    }
+
     public int getCargasDraconicas() {
         return cargasDraconicas;
     }
 }
+

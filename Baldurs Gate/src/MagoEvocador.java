b5804c48e2e6267e4f48e20089b140fa743d71cd
Acarphia <76790334+Acarphia@users.noreply.github.com>
2025-08-02 18:53:54 -0300

    Update MagoEvocador.java

--- Baldurs Gate/src/MagoEvocador.java
+++ Baldurs Gate/src/MagoEvocador.java
@@ -1,18 +1,31 @@
+// Subclasse Mago Evocador (herda de Mago)
 public class MagoEvocador extends Mago {
-    private int evocacoes;
+    private int evocacoes; // Cargas da habilidade Evocar Elemental
 
     public MagoEvocador(Mago mago) {
         super(mago);
-        this.evocacoes = 2;
+        this.evocacoes = 2; // Inicia com 2 cargas
         this.habilidades.add("Evocar Elemental (2 cargas)");
     }
 
+    // Método único da subclasse
     public void evocarElemental() {
         if (evocacoes > 0) {
             evocacoes--;
             System.out.println(nome + " evoca um elemental para a batalha!");
         } else {
             System.out.println(nome + " não tem evocação suficiente!");
+        }
+    }
+
+    // Sobrescrita
+    @Override
+    public void lancarMisseis() {
+        if (mana >= 10) {
+            mana -= 10;
+            System.out.println(nome + " lança mísseis mágicos com poder redobrado!");
+        } else {
+            System.out.println(nome + " não tem mana suficiente para lançar mísseis!");
         }
     }
 

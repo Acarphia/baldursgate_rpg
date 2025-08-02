e6f0ff6169b196a6dffd18241befe9bffe6c73d9
Acarphia <76790334+Acarphia@users.noreply.github.com>
2025-08-02 18:52:07 -0300

    Update GuerreiroCampeao.java

--- Baldurs Gate/src/GuerreiroCampeao.java
+++ Baldurs Gate/src/GuerreiroCampeao.java
@@ -1,18 +1,31 @@
+// Subclasse Guerreiro Campeão (herda de Guerreiro)
 public class GuerreiroCampeão extends Guerreiro {
-    private int golpesCriticos;
+    private int golpesCriticos; // Cargas da habilidade Golpe Crítico
 
     public GuerreiroCampeão(Guerreiro guerreiro) {
         super(guerreiro);
-        this.golpesCriticos = 1;
+        this.golpesCriticos = 1; // Inicia com 1 carga
         this.habilidades.add("Golpe Crítico (1 carga)");
     }
 
+    // Método único de subclasse
     public void usarGolpeCritico() {
         if (golpesCriticos > 0) {
             golpesCriticos--;
             System.out.println(nome + " executa um golpe crítico devastador!");
         } else {
             System.out.println(nome + " não tem golpes críticos restantes!");
+        }
+    }
+
+    // Sobrescrita
+    @Override
+    public void usarFuria() {
+        if (golpesCriticos > 0) {
+            golpesCriticos--;
+            System.out.println(nome + " canaliza a fúria em um golpe crítico inigualável!");
+        } else {
+            System.out.println(nome + " tenta se enfurecer, mas não tem energia para um golpe decisivo!");
         }
     }
 
